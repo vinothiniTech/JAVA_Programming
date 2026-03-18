7428b01fe00b990e80fdfa3d7fbea038d7e5549a
Vinothini Sivakumar <vinothinis357@gmail.com>
2026-03-18 21:16:39 +0530

    Add files via upload

---
+++ conditional statements/example.java
@@ -0,0 +1,11 @@
+public class example{
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
